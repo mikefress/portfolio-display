42bf744b7de8fc26bcc6c2fa0fc9ff6953f01a17
Michael Fresson <65670932+mikefress@users.noreply.github.com>
2023-10-04 16:07:36 +0100

    getting there

---
+++ src/data/portfolioItems.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    type: 'Banner',
+    time: 15,
+    width: 160,
+    height: 600,
+    name: 'Playstation Game Range 2023',
+    description: 'Part of the Game range of Banners made for PlayStation to bring awareness to the wide range of AAA games available on the platform, this Banner utilises animated clip paths to create the fan effect, allowing the developer to whiteout large sections of the images, saving fileweight.',
+    source: 'https://api-campaigns.cainandabelddb.com/preview/5/bcfd61ea-5149-427b-b23d-5e7f2ea89206/a9dde2e5-f50b-4a44-9b55-41650c6f7a2c/',
+  },
+  {
+    type: 'Banner',
+    time: 15,
+    width: 640,
+    height: 480,
+    name: 'Cisco Security 2023',
+    description: 'Cisco’s banner work has often been fairly simple, but this time we were challenged to create something that would reflect their video pieces and really catch the eye. Originally designed to be reversed from how you see it, we went back to the drawing board to find a solution that the client loved, even showing it to their CEO.',
+    source: 'https://api-campaigns.cainandabelddb.com/preview/19/68bf868d-ed69-480e-a710-62eb2a007a23/357abfe4-bf94-4bdd-a7ac-7faeb1406d62/',
+  },
+];
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,111 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section>
+    <img id="logo" src="https://campaigns.cainandabelddb.com/img/logo.796b4e7a.svg" alt="cain&abelDDB logo">
+    <div class="home">
+      <div id="heading">
+        <div id="title" ref="title">Banner1</div>
+
+        <div id="description" ref="description">blah blah balh</div>
+      </div>
+      <div id="mediaHolder" ref="mediaHolder">
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import portfolio from '@/data/portfolioItems';
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld,
+  },
+  data() {
+    return {
+      mediaPlayer: null,
+    };
+  },
+  methods: {
+    checkAndSetMediaPlayer(item) {
+      if (item.type === 'Banner') {
+        this.mediaPlayer = document.createElement('iframe');
+        this.mediaPlayer.setAttribute('src', item.source);
+        this.mediaPlayer.setAttribute('height', item.height);
+        this.mediaPlayer.setAttribute('width', item.width);
+        this.mediaPlayer.setAttribute('frameBorder', 0);
+        this.$refs.mediaHolder.appendChild(this.mediaPlayer);
+      }
+    },
+    runPortfolio() {
+      console.log(portfolio);
+      // console.log(this.$refs.title.innerHTML);
+      let counter = 0;
+      this.$refs.title.innerHTML = portfolio[counter].name;
+      this.$refs.description.innerHTML = portfolio[counter].description;
+      this.checkAndSetMediaPlayer(portfolio[counter]);
+      counter += 1;
+      // set Initial value
+      setInterval(() => {
+        // tear down previous work
+        this.mediaPlayer.remove();
+        this.mediaPlayer = null;
+        // Set title and description
+        this.$refs.title.innerHTML = portfolio[counter].name;
+        this.$refs.description.innerHTML = portfolio[counter].description;
+        // actually mount the bloody work
+        this.checkAndSetMediaPlayer(portfolio[counter]);
+
+        // use counter to loop around array.
+        counter += 1;
+        if (counter > portfolio.length - 1) {
+          counter = 0;
+        }
+      }, (portfolio[counter].time * 1000 + 2000));
+    },
+  },
+  mounted() {
+    this.runPortfolio();
   },
 };
 </script>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  max-width: 1200px;
+  /* align-items: center; */
+  margin: 0 auto;
+}
+
+#logo {
+  position: absolute;
+  bottom: 1vh;
+  right: 1vw;
+  width: 100px;
+  height: auto;
+}
+
+#heading {
+  max-width: 1200px;
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+}
+
+#title {
+  font-size: 40px;
+  line-height: 1.2;
+  letter-spacing: 0.1em;
+  font-weight: 600;
+  margin-bottom: 1rem;
+}
+
+#description {
+  font-size: 24px;
+}
+
+</style>
